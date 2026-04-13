1f4812f0458c00ce1ed0db344bf6fd181fbfafa7
thom98592 <thom98592@gmail.com>
2026-04-13 11:22:48 +0530

    Notes-api app

--- notes-api/src/main/java/com/example/demo/exceptions/GlobalExceptionHandler.java
+++ notes-api/src/main/java/com/example/demo/exceptions/GlobalExceptionHandler.java
@@ -6,22 +6,27 @@ import java.util.Map;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestController
+@RestControllerAdvice
 public class GlobalExceptionHandler {
-	@ExceptionHandler(Exception.class)
-	public String handleExceptions(Exception ex) {
-		return "Something Went Wrong";
-	}
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
-		Map<String, String> errors = new HashMap<>();
-		ex.getBindingResult().getAllErrors().forEach((error) -> {
-			String fieldName = ((FieldError) error).getField();
-			String errorMessage = error.getDefaultMessage();
-			errors.put(fieldName, errorMessage);
-		});
-		return errors;
-	}
+
+    @ExceptionHandler(Exception.class)
+    public String handleExceptions(Exception ex) {
+        return "Something Went Wrong";
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return errors;
+    }
 }
